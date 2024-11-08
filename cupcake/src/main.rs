db65020584cd1080e50f7130ee36a8e1161a8fe3
Matteo Foccoli <matteo.foccoli@gmail.com>
2024-11-08 14:38:18 +0100

    chocolate cupcake

--- cupcake/src/main.rs
+++ cupcake/src/main.rs
@@ -2,15 +2,42 @@ fn main() {
     println!("Hello, world!");
 }
 
-struct Cupcake {
+trait Cupcake {
+    fn print(&self) -> String;
+}
+
+trait Topping {
+    fn new(cupcake: Box<dyn Cupcake>) -> impl Cupcake;
+}
+
+struct PlainCupcake {
 
 }
 
-impl Cupcake {
+impl Cupcake for PlainCupcake {
     fn print(&self) -> String {
         "🧁".to_string()
     }
 }
+
+struct ChocolateCupcake {
+    cupcake: Box<dyn Cupcake>,
+}
+
+impl Cupcake for ChocolateCupcake {
+    fn print(&self) -> String {
+        return format!("{} with 🍫", self.cupcake.print())
+    }
+}
+
+impl Topping for ChocolateCupcake {
+    fn new(cupcake: Box<dyn Cupcake>) -> impl Cupcake {
+        ChocolateCupcake {
+            cupcake
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -18,12 +45,20 @@ mod test {
     use super::*;
 
     #[test]
-    fn creats_a_plain_cupcake() {
-        let cupcake = Cupcake {};
+    fn creates_a_plain_cupcake() {
+        let cupcake = PlainCupcake {};
 
         let result = cupcake.print();
 
         assert_eq!("🧁", result);
     }
 
+    #[test]
+    fn puts_some_chocolate_on_top() {
+        let cupcake = ChocolateCupcake::new(Box::new(PlainCupcake {}));
+
+        let result = cupcake.print();
+
+        assert_eq!("🧁 with 🍫", result);
+    }
 }
