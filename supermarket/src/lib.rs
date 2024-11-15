ca47066aadecdce2c41db9694acd9f3bd3c34f7b
Matteo Foccoli <matteo.foccoli@gmail.com>
2024-11-15 08:30:01 +0100

    first pricing rule

--- supermarket/src/lib.rs
+++ supermarket/src/lib.rs
@@ -1,4 +1,5 @@
 struct CheckOut {
+    pricing_rule: PricingRule,
     items: Vec<Item>
 }
 
@@ -7,9 +8,25 @@ struct Item {
     unit_price: f32,
 }
 
+struct PricingRule {
+    sku: String,
+}
+
+impl PricingRule {
+    fn discount(&self, items: &Vec<Item>) -> f32 {
+        let discountable_items_count = items.iter().filter(|i| i.sku == "A".to_string()).count();
+        if discountable_items_count > 1 {
+            return 5.0;
+        } else {
+            return 0.0;
+        }
+    }
+}
+
 impl CheckOut {
-    fn new() -> Self {
+    fn new(pricing_rule: PricingRule) -> Self {
         Self {
+            pricing_rule,
             items: vec!()
         }
     }
@@ -18,7 +35,9 @@ impl CheckOut {
     }
 
     fn total(&self) -> f32 {
-        self.items.iter().map(|i| i.unit_price).sum()
+        let mut total = self.items.iter().map(|i| i.unit_price).sum();
+        total -= self.pricing_rule.discount(&self.items);
+        total
     }
 }
 
@@ -28,20 +47,30 @@ mod tests {
 
     #[test]
     fn one_item_in_the_cart() {
-        let mut check_out = CheckOut::new();
+        let mut check_out = CheckOut::new(PricingRule { sku: "A".to_string() });
 
         check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
 
-        assert_eq!(10.0, check_out.total())
+        assert_eq!(10.0, check_out.total());
     }
 
     #[test]
     fn two_items_in_the_cart() {
-        let mut check_out = CheckOut::new();
+        let mut check_out = CheckOut::new(PricingRule {sku: "A".to_string()});
 
         check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
         check_out.scan(Item {sku: "B".to_string(), unit_price: 20.0});
 
-        assert_eq!(30.0, check_out.total())
+        assert_eq!(30.0, check_out.total());
+    }
+
+    #[test]
+    fn special_offer_on_an_item() {
+        let mut check_out = CheckOut::new(PricingRule {sku: "A".to_string()});
+
+        check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
+        check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
+
+        assert_eq!(15.0, check_out.total());
     }
 }
