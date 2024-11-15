d1f9b83996bda905584be4a1651f061fdb25fa39
Matteo Foccoli <matteo.foccoli@gmail.com>
2024-11-15 08:03:40 +0100

    cart with two items

--- supermarket/src/lib.rs
+++ supermarket/src/lib.rs
@@ -1,4 +1,6 @@
-struct CheckOut {}
+struct CheckOut {
+    items: Vec<Item>
+}
 
 struct Item {
     sku: String,
@@ -6,12 +8,17 @@ struct Item {
 }
 
 impl CheckOut {
-    fn scan(&self, item: Item) {
-
+    fn new() -> Self {
+        Self {
+            items: vec!()
+        }
+    }
+    fn scan(&mut self, item: Item) {
+        self.items.push(item);
     }
 
     fn total(&self) -> f32 {
-        10.0
+        self.items.iter().map(|i| i.unit_price).sum()
     }
 }
 
@@ -21,10 +28,20 @@ mod tests {
 
     #[test]
     fn one_item_in_the_cart() {
-        let check_out = CheckOut {};
+        let mut check_out = CheckOut::new();
 
         check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
 
         assert_eq!(10.0, check_out.total())
+    }
+
+    #[test]
+    fn two_items_in_the_cart() {
+        let mut check_out = CheckOut::new();
+
+        check_out.scan(Item {sku: "A".to_string(), unit_price: 10.0});
+        check_out.scan(Item {sku: "B".to_string(), unit_price: 20.0});
+
+        assert_eq!(30.0, check_out.total())
     }
 }
