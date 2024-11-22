dadfdfee627cdccb0108c79a9ce646118def219c
Matteo Foccoli <matteo.foccoli@gmail.com>
2024-11-22 18:10:40 +0100

    calculated balance and adds one row per each deposit in the statement

--- banking/src/lib.rs
+++ banking/src/lib.rs
@@ -1,8 +1,13 @@
+use chrono::{DateTime, Utc};
+
 struct Account {
     transactions: Vec<Transaction>,
 }
 
-struct Transaction;
+struct Transaction {
+    timestamp: DateTime<Utc>,
+    amount: f32,
+}
 
 impl Account {
     fn new() -> Self {
@@ -11,22 +16,35 @@ impl Account {
         }
     }
 
+    fn deposit(&mut self, amount: f32, timestamp: DateTime<Utc>) {
+        self.transactions.push(Transaction { timestamp, amount })
+    }
+
     fn print_statement(&self) -> String {
+        let mut balance = 0.0;
         let mut body = "".to_owned();
         body.push_str("Date        Amount  Balance");
         for transaction in &self.transactions {
-            body.push_str("\n24.12.2015   +500      500")
+            balance += transaction.amount;
+            body.push_str(&self.print_transaction_line(&transaction, balance));
         }
         body
     }
 
-    fn deposit(&mut self, amount: f32) {
-        self.transactions.push(Transaction)
+    fn print_transaction_line(&self, transaction: &Transaction, current_balance: f32) -> String {
+        format!(
+            "\n{}   +{}      {}",
+            transaction.timestamp.format("%d.%m.%Y"),
+            transaction.amount,
+            current_balance
+        )
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use chrono::NaiveDate;
+
     use super::*;
 
     #[test]
@@ -41,7 +59,7 @@ mod tests {
     #[test]
     fn prints_statement_with_one_deposit_only() {
         let mut account = Account::new();
-        account.deposit(10.0);
+        account.deposit(500.0, parse_date("2015-12-24"));
 
         let statement = account.print_statement();
 
@@ -51,17 +69,25 @@ mod tests {
         assert_eq!(expected_statement, statement);
     }
 
+    #[test]
     fn prints_statement_with_two_deposits() {
         let mut account = Account::new();
-        account.deposit(10.0);
-        account.deposit(20.0);
+        account.deposit(10.0, parse_date("2015-12-22"));
+        account.deposit(20.0, parse_date("2015-12-24"));
 
         let statement = account.print_statement();
 
         let expected_statement = "Date        Amount  Balance\n\
-            24.12.2015   +10      10\n\
+            22.12.2015   +10      10\n\
             24.12.2015   +20      30"
             .to_string();
         assert_eq!(expected_statement, statement);
+    }
+
+    fn parse_date(date_string: &str) -> DateTime<Utc> {
+        let date_string = &format!("{} 00:00:00.000 +0000", date_string);
+        let date_format = "%Y-%m-%d %H:%M:%S%.3f %z";
+        let result = DateTime::parse_from_str(date_string, date_format);
+        result.unwrap().into()
     }
 }
