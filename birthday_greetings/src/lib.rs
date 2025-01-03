1ee126f00a06ed9edb7bc0afe2932602f00a84d5
Matteo Foccoli <matteo.foccoli@gmail.com>
2025-01-03 16:42:44 +0100

    sends greeting only if today is their birthday

--- birthday_greetings/src/lib.rs
+++ birthday_greetings/src/lib.rs
@@ -1,9 +1,9 @@
-use chrono::NaiveDate;
+use chrono::{Datelike, NaiveDate};
 use mockall::automock;
 
 mod file_repository;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Contact {
     pub first_name: String,
     pub last_name: String,
@@ -13,7 +13,7 @@ pub struct Contact {
 
 #[automock]
 pub trait Sender {
-    fn send(&self, contacts: Vec<Contact>) -> Result<usize, String>;
+    fn send(&self, contact: Contact) -> Result<(), String>;
 }
 
 #[automock]
@@ -24,20 +24,37 @@ pub trait Repository {
 pub struct Greeter {
     sender: Box<dyn Sender>,
     repository: Box<dyn Repository>,
+    today: NaiveDate,
 }
 
 impl Greeter {
-    pub fn new(sender: Box<dyn Sender>, repository: Box<dyn Repository>) -> Self {
-        Self { sender, repository }
+    pub fn new(sender: Box<dyn Sender>, repository: Box<dyn Repository>, today: NaiveDate) -> Self {
+        Self {
+            sender,
+            repository,
+            today,
+        }
     }
 
     pub fn send_greetings(&self) -> Result<String, String> {
         let contacts = self.repository.load()?;
         let mut sent_messages = 0;
         if let Some(contacts) = contacts {
-            sent_messages = self.sender.send(contacts)?;
+            for contact in contacts {
+                if self.today_is_their_birthday(&contact) {
+                    match self.sender.send(contact) {
+                        Ok(_) => sent_messages += 1,
+                        Err(e) => return Err(e),
+                    }
+                }
+            }
         }
-        Ok(format!("Sent {sent_messages} message"))
+        Ok(format!("Sent {sent_messages} messages"))
+    }
+
+    fn today_is_their_birthday(&self, contact: &Contact) -> bool {
+        contact.date_of_birth.day0() == self.today.day0()
+            && contact.date_of_birth.month0() == self.today.month0()
     }
 }
 
@@ -48,24 +65,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn successfully_greets() {
+    fn successfully_greets_on_date_of_birth() {
+        let contacts = contacts();
+
         let mut sender = MockSender::new();
         sender
             .expect_send()
-            .with(eq(contacts()))
+            .with(eq(contacts[0].clone()))
             .once()
-            .returning(|_| Ok(contacts().len()));
+            .returning(|_| Ok(()));
 
         let mut repository = MockRepository::new();
         repository
             .expect_load()
             .once()
-            .return_once(|| Ok(Some(contacts())));
+            .return_once(|| Ok(Some(contacts)));
 
-        let greeter = Greeter::new(Box::new(sender), Box::new(repository));
+        let greeter = Greeter::new(Box::new(sender), Box::new(repository), today());
         let result = greeter.send_greetings();
 
-        assert_eq!(result, Ok("Sent 2 message".to_string()));
+        assert_eq!(result, Ok("Sent 1 messages".to_string()));
+    }
+
+    fn today() -> NaiveDate {
+        NaiveDate::parse_from_str("2025/10/08", "%Y/%m/%d").unwrap()
     }
 
     #[test]
@@ -82,7 +105,7 @@ mod tests {
             .once()
             .return_once(|| Ok(Some(contacts())));
 
-        let greeter = Greeter::new(Box::new(sender), Box::new(repository));
+        let greeter = Greeter::new(Box::new(sender), Box::new(repository), today());
         let result = greeter.send_greetings();
 
         assert_eq!(result, Err("Error sending".to_string()));
@@ -99,7 +122,7 @@ mod tests {
             .once()
             .return_once(|| Err("Error loading contacts".to_string()));
 
-        let greeter = Greeter::new(Box::new(sender), Box::new(repository));
+        let greeter = Greeter::new(Box::new(sender), Box::new(repository), today());
         let result = greeter.send_greetings();
 
         assert_eq!(result, Err("Error loading contacts".to_string()));
@@ -113,10 +136,10 @@ mod tests {
         let mut repository = MockRepository::new();
         repository.expect_load().once().return_once(|| Ok(None));
 
-        let greeter = Greeter::new(Box::new(sender), Box::new(repository));
+        let greeter = Greeter::new(Box::new(sender), Box::new(repository), today());
         let result = greeter.send_greetings();
 
-        assert_eq!(result, Ok("Sent 0 message".to_string()));
+        assert_eq!(result, Ok("Sent 0 messages".to_string()));
     }
 
     fn contacts() -> Vec<Contact> {
@@ -125,13 +148,13 @@ mod tests {
                 first_name: "Matteo".to_string(),
                 last_name: "Foo".to_string(),
                 email: "matteo@test.com".to_string(),
-                date_of_birth: NaiveDate::parse_from_str("2015-09-05", "%Y-%m-%d").unwrap(),
+                date_of_birth: NaiveDate::parse_from_str("1982/10/08", "%Y/%m/%d").unwrap(),
             },
             Contact {
                 first_name: "Lily".to_string(),
                 last_name: "Bar".to_string(),
                 email: "lily@test.com".to_string(),
-                date_of_birth: NaiveDate::parse_from_str("2015-09-05", "%Y-%m-%d").unwrap(),
+                date_of_birth: NaiveDate::parse_from_str("1943/11/22", "%Y/%m/%d").unwrap(),
             },
         ]
     }
