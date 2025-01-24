ede73bd19f6ba24514f6ab17df1851e1108bb7f5
Matteo Foccoli <matteo.foccoli@gmail.com>
2025-01-24 08:38:32 +0100

    modelling tennis match

--- tennis/src/lib.rs
+++ tennis/src/lib.rs
@@ -6,15 +6,66 @@ impl Scoreboard {
     }
 }
 
+pub struct Match {}
+
+impl Match {
+    pub fn score(&self) -> Score {
+        return Score {
+            first_player: Player {
+                name: "Pete".to_string(),
+            },
+            first_player_score: Points::Zero,
+            second_player: Player {
+                name: "Boris".to_string(),
+            },
+            second_player_score: Points::Zero,
+        };
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Score {
+    pub first_player: Player,
+    pub first_player_score: Points,
+    pub second_player: Player,
+    pub second_player_score: Points,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Player {
+    pub name: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Points {
+    Zero,
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn game_started() {
+    fn scoreboard_displays_initial_score() {
         let scoreboard = Scoreboard {};
 
         let expected_result = "Player A: 0, Player B: 0";
         assert_eq!(expected_result, scoreboard.display());
+    }
+
+    #[test]
+    fn match_has_started() {
+        let current_match = Match {};
+
+        let expected_score = Score {
+            first_player: Player {
+                name: "Pete".to_string(),
+            },
+            first_player_score: Points::Zero,
+            second_player: Player {
+                name: "Boris".to_string(),
+            },
+            second_player_score: Points::Zero,
+        };
+        assert_eq!(expected_score, current_match.score());
     }
 }
