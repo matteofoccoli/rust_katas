b790d3062a5b6ecc12b6e853296060ecf4a5d797
Matteo Foccoli <matteo.foccoli@gmail.com>
2025-02-08 16:30:04 +0100

    parses accounts with 0s and 1s

--- bank_ocr/src/lib.rs
+++ bank_ocr/src/lib.rs
@@ -2,44 +2,61 @@ use std::fs::read_to_string;
 
 pub struct Parser {}
 
-const LINE_BUFFER_WIDTH: usize = 27;
-const LINE_BUFFER_HEIGHT: usize = 3;
-const NUMBER_BUFFER_WIDTH: usize = 3;
-const NUMBER_BUFFER_HEIGHT: usize = 3;
+const ACCOUNT_NUMBER_BUFFER_COLUMNS: usize = 27;
+const ACCOUNT_NUMBER_BUFFER_ROWS: usize = 3;
+const ACCOUNT_NUMBER_BUFFER_MAX_ROWS: usize = 4;
+const NUMBER_BUFFER_COLUMNS: usize = 3;
+const NUMBER_BUFFER_ROWS: usize = 3;
 
 #[derive(PartialEq, Debug)]
 pub struct AccountNumber {
-    number: [u8; 9],
+    numbers: [u8; 9],
 }
 
 impl Parser {
     pub fn parse(&self, file_path: String) -> Vec<AccountNumber> {
         let file_content = read_to_string(file_path).unwrap();
-        let mut line_buffer = [[' '; LINE_BUFFER_WIDTH]; LINE_BUFFER_HEIGHT];
-        let mut number_buffer = [[' '; NUMBER_BUFFER_WIDTH]; NUMBER_BUFFER_HEIGHT];
+        let mut line_buffer = [[' '; ACCOUNT_NUMBER_BUFFER_COLUMNS]; ACCOUNT_NUMBER_BUFFER_ROWS];
         let mut buffer_line_index;
+        let mut account_numbers: Vec<AccountNumber> = vec![];
 
         for (line_index, line) in file_content.lines().into_iter().enumerate() {
-            buffer_line_index = line_index % 4;
-            if buffer_line_index == 3 {
-                println!("> Reached empty line {}, processing buffer", line_index);
-                // TODO processare il contenuto del buffer a questo punto
-                println!("Buffer content is: {:?}", line_buffer);
+            buffer_line_index = line_index % ACCOUNT_NUMBER_BUFFER_MAX_ROWS;
+            if buffer_line_index == ACCOUNT_NUMBER_BUFFER_ROWS {
+                account_numbers.push(parse_account_number_line(line_buffer.clone()));
                 break;
             }
-            println!("> Parsing line {}, content is: '{}'", line_index, line);
             for (current_char_index, current_char) in line.chars().into_iter().enumerate() {
                 line_buffer[buffer_line_index][current_char_index] = current_char;
             }
-            println!(
-                "> Buffer line {}, content is: {:?}",
-                buffer_line_index, line_buffer[buffer_line_index]
-            );
         }
+        account_numbers
+    }
+}
 
-        vec![AccountNumber {
-            number: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        }]
+fn parse_account_number_line(
+    line_buffer: [[char; ACCOUNT_NUMBER_BUFFER_COLUMNS]; ACCOUNT_NUMBER_BUFFER_ROWS],
+) -> AccountNumber {
+    let mut number_buffer = [[' '; NUMBER_BUFFER_COLUMNS]; NUMBER_BUFFER_ROWS];
+    let mut numbers = [9; 9];
+    for offset in (0..ACCOUNT_NUMBER_BUFFER_COLUMNS).step_by(NUMBER_BUFFER_COLUMNS) {
+        for row in 0..NUMBER_BUFFER_ROWS {
+            for column in offset..offset + NUMBER_BUFFER_COLUMNS {
+                number_buffer[row][column - offset] = line_buffer[row][column];
+            }
+        }
+        let number = convert(number_buffer);
+        numbers[offset / 3] = number;
+    }
+    println!("Numbers is {:?}", numbers);
+    AccountNumber { numbers }
+}
+
+fn convert(buffer: [[char; 3]; 3]) -> u8 {
+    match buffer {
+        [[' ', '_', ' '], ['|', ' ', '|'], ['|', '_', '|']] => 0,
+        [[' ', ' ', ' '], [' ', ' ', '|'], [' ', ' ', '|']] => 1,
+        _ => todo!(),
     }
 }
 
@@ -48,16 +65,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simple_parsing() {
+    fn one_account_number_of_zeros() {
         let parser = Parser {};
 
-        let result = parser.parse("./accounts.txt".to_string());
+        let result = parser.parse("./all_0s_account_number.txt".to_string());
 
-        assert_eq!(
-            vec!(AccountNumber {
-                number: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            }),
-            result
-        );
+        assert_eq!(vec!(AccountNumber { numbers: [0; 9] }), result);
+    }
+
+    #[test]
+    fn one_account_number_of_ones() {
+        let parser = Parser {};
+
+        let result = parser.parse("./all_1s_account_number.txt".to_string());
+
+        assert_eq!(vec!(AccountNumber { numbers: [1; 9] }), result);
     }
 }
