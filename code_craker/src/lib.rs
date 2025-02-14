9ec75da9f0b5d50f457a7bd6c540336432204691
Matteo Foccoli <matteo.foccoli@gmail.com>
2025-02-14 15:33:42 +0100

    encrypts two chars

--- code_craker/src/lib.rs
+++ code_craker/src/lib.rs
@@ -1,8 +1,24 @@
 pub struct CodeCracker;
 
 impl CodeCracker {
-    pub fn encrypt(&self, _: &str) -> &str {
-        "!"
+    pub fn encrypt(&self, value: &str) -> String {
+        let decrypted = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        let encrypted = [
+            '!', ')', '"', '(', '£', '*', '%', '&', '>', '<', '@', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+        ];
+        let mut result = String::new();
+        for char in value.chars() {
+            let position = decrypted.into_iter().position(|x| x == char);
+            match position {
+                Some(position) => result.push(encrypted[position]),
+                None => result.push(char),
+            }
+        }
+        result
     }
 }
 
@@ -17,5 +33,14 @@ mod tests {
         let result = code_cracker.encrypt("a");
 
         assert_eq!("!", result);
+    }
+
+    #[test]
+    fn encrypts_two_chars() {
+        let code_cracker = CodeCracker {};
+
+        let result = code_cracker.encrypt("ab");
+
+        assert_eq!("!)", result);
     }
 }
